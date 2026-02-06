1e05b76d67c7e448e7d1d008e81466afd0761372
tinytony55 <118210931+tinytony55@users.noreply.github.com>
2026-02-06 15:20:42 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,17 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+   // toggleGrid();
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(1100, 599, 200, 10, "orange");
+    createPlatform(650, 485, 200, 10, "red");
     // TODO 3 - Create Collectables
-
-
-
-    
+    createCollectable("database", 1200, 550, 0, 0.7);
+    createCollectable("database", 740, 440, 0, 0.7);
     // TODO 4 - Create Cannons
-
-
-    
-    
+    createCannon("top", 200, 600);
+    createCannon("right", 400, 600);
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
